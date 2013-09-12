246f581f93c789bf6c98fd741c7d8731c227e436
Daniel Damgaard <daniel@damgaard.in>
2013-09-12 15:32:33 +0200

    Indledende test

---
+++ dk/au/cs/eagleEye/DistanceTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.au.cs.eagleEye;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Daniel
+ */
+public class DistanceTest {
+  
+  public DistanceTest() {
+  }
+  
+  @BeforeClass
+  public static void setUpClass() {
+  }
+  
+  @AfterClass
+  public static void tearDownClass() {
+  }
+  
+  @Before
+  public void setUp() {
+  }
+  
+  @After
+  public void tearDown() {
+  }
+  
+  @Test
+  public void hello() {
+    assertEquals("Lad os se om junit virker", 4, 4);
+  }
+}
